e8e270f1c6f7961b81733b5511432cb4ee22e883
VincentLu91 <vincentlu@hotmail.ca>
2013-07-20 16:29:49 -0400

    Create math_exercise.c

---
+++ math_exercise.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 3 + 4;
+	int b = (5*a - 3) + 21;
+	printf("The value that a holds is: %d\n", a);
+	printf("The value that b holds is: %d\n", b);
+	printf("What does a + b equal to? %d\n", a + b);
+	return 0;
+}
